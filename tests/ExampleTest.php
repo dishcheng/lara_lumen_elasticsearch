3d2383cde6c6a554a5c60837dd4375bbd71b9ffb
caicheng <caicheng@hooook.com>
2020-07-23 17:25:27 +0800

    update test

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,9 +1,11 @@
 <?php
 
+namespace DishCheng\LaraLumenElasticSearch\Tests;
+
 class ExampleTest extends TestCase
 {
     public function testData()
     {
-        $this->assertEquals('a','a');
+        $this->assertEquals('a', 'a');
     }
 }
---
+++ tests/TestCase.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * This file is part of the overtrue/easy-sms.
+ *
+ * (c) overtrue <i@overtrue.me>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace DishCheng\LaraLumenElasticSearch\Tests;
+
+class TestCase extends \PHPUnit\Framework\TestCase
+{
+}
